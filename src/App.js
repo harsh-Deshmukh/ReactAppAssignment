75929b322dfc30b3edd098db13a7e07a560997a2
HD <harshal.deshmukh02@gmail.com>
2020-12-03 08:24:24 +0530

    First react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+
+      <div className="App">
+        <h1>This is MY first react app</h1>
+        <p>I want to develop app using React</p>
+      </div>
+    );
+  }
+}
+
+export default App;
